e1bb1f12ccd27631ee22a3127f83f0cf7d1d03f1
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 12:07:04 -0700

    adding nav and search to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import ListingList from '../listings/ListingList';
+'use client';
+
+import Navigation from '@/components/nav';
+import ListingList from '../components/listings/ListingList';
+import SearchBar from '../components/searchBar';
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(' ');
+}
 
 export default function Home() {
-	return <ListingList />;
+	return (
+		<>
+			<div className='Home min-h-full'>
+				<Navigation />
+				<header className='bg-slate-100 shadow'>
+					<div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+						<SearchBar />
+					</div>
+				</header>
+				<main>
+					<div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>
+						<ListingList />
+					</div>
+				</main>
+			</div>
+		</>
+	);
 }
