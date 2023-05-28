2811030c02a43834dcd233bf8212b8780b768c24
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-27 17:55:29 -0700

    s3 support in next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     remotePatterns: [{
       protocol: 'https',
       hostname: 'placehold.co'
+    },
+    {
+      protocol: process.env.NEXT_PUBLIC_S3_PROTOCOL,
+      hostname: process.env.NEXT_PUBLIC_S3_HOSTNAME,
+      port: '',
+      pathname: `/${process.env.NEXT_PUBLIC_S3_BUCKET}/**`
     }]
   }
 }
